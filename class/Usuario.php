63c8a673506d04dd1a64122065cff3b14c4c97c0
Lucas Correa <lucas.nhl@gmail.com>
2017-04-02 23:36:17 -0300

    Inserir e Alterar

--- class/Usuario.php
+++ class/Usuario.php
@@ -9,8 +9,12 @@ class Usuario {
 	private $data_criacao;
 
 	// Construtor
-	public function __construct() {
+	// public function __construct() {
 
+	// }
+	public function __construct($login="", $senha="") {
+		$this->setLogin($login);
+		$this->setSenha($senha);
 	}
 
 	// GETs e SETs
@@ -62,16 +66,84 @@ class Usuario {
 			));
 
 		if ( count($results) > 0 ) {
-			$row = $results[0];
-
-			$this->setIdUsuario($row['id_usuario']);
-			$this->setLogin($row['login']);
-			$this->setSenha($row['senha']);
-			$this->setDataCriacao(new DateTime($row['data_cadastro']));
+			$this->setData($results[0]);
 		}
 
 	}
 
+	public function login($login, $senha) {
+
+		$sql = new Sql();
+
+		$results = $sql->select("SELECT * FROM `tb_usuario` WHERE `login` = :LOGIN AND `senha` = :SENHA", array(
+			':LOGIN' => $login,
+			':SENHA' => $senha
+			));
+
+		if ( count($results) > 0 ) {
+			$this->setData($results[0]);
+		} else {
+			throw new Exception("Login e/ou Senha inválidos!", 1);
+		}
+
+	}
+
+	private function setData($data) {
+		$this->setIdUsuario($data['id_usuario']);
+		$this->setLogin($data['login']);
+		$this->setSenha($data['senha']);
+		$this->setDataCriacao(new DateTime($data['data_cadastro']));
+	}
+
+	public static function getList() {
+
+		$sql = new Sql();
+		return $sql->select("SELECT * FROM `tb_usuario` ORDER BY `login`");
+
+	}
+
+	public static function search($login) {
+
+		$sql = new Sql();
+		return $sql->select("SELECT * FROM `tb_usuario` WHERE `login` LIKE :SEARCH ORDER BY `login`", array(
+			':SEARCH' => "%". $login ."%"
+			));
+
+	}
+
+	public function insert() {
+
+		$sql = new Sql();
+
+		$results = $sql->select("CALL sp_usuario_insert(:LOGIN, :SENHA)", array(
+			':LOGIN' => $this->getLogin(),
+			':SENHA' => $this->getSenha()
+			));
+
+		if ( count($results) > 0 ) {
+			$this->setData($results[0]);
+		} else {
+			throw new Exception("Login e/ou Senha inválidos!", 1);
+		}
+
+	}
+
+	public function update($login, $senha) {
+
+		$this->setLogin($login);
+		$this->setSenha($senha);
+
+		$sql = new Sql();
+
+		$sql->select("UPDATE `tb_usuario` SET `login` = :LOGIN, `senha` = :SENHA WHERE `id_usuario` = :ID", array(
+			':LOGIN' => $this->getLogin(),
+			':SENHA' => $this->getSenha(),
+			':ID' => $this->getIdUsuario()
+			));
+
+	}
+
+	// Metodos Magicos
 	public function __toString() {
 		return json_encode(array(
 			'id_usuario' => $this->getIdUsuario(),
--- index.php
+++ index.php
@@ -4,9 +4,33 @@ require_once("config.php");
 
 $sql = new Sql();
 
-$user = new Usuario();
-$user->loadById(3);
+// Carrega 1 Usuario
+// $user = new Usuario();
+// $user->loadById(3);
+// echo $user;
 
+// Carrega Lista de Usuarios
+// $lista = Usuario::getList();
+// echo json_encode($lista);
+
+// Busca Usuario pelo login
+// $search = Usuario::search("root");
+// echo json_encode($search);
+
+// Valida Login
+// $user = new Usuario();
+// $user->login("root", "qwertysa");
+// echo $user;
+
+// Insere
+// $aluno = new Usuario("aluno", "@lun0");
+// $aluno->insert();
+// echo $aluno;
+
+// Update
+$user = new Usuario();
+$user->loadById(6);
+$user->update("professor", "profs");
 echo $user;
 
  ?>
